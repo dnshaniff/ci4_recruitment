fb426a80b78da58168704d0f3aa9c474a9ac3633
Denis Hanif <dnshaniff@gmail.com>
2026-05-11 22:32:30 +0700

    initial CI4, setup users migration and seeder admin

---
+++ app/Database/Seeds/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        $this->db->table('users')->insert([
+            'name' => 'Administrator',
+            'email' => 'admin@mail.com',
+            'password' => password_hash('P@ssw0rd', PASSWORD_DEFAULT),
+            'status' => 'active',
+            'created_at' => date('Y-m-d H:i:s'),
+            'created_by' => 1,
+        ]);
+    }
+}
---
+++ app/Models/UserModel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+    protected $table            = 'users';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'name',
+        'email',
+        'password',
+        'status',
+        'created_by',
+        'updated_by',
+        'deleted_by'
+    ];
+
+    protected bool $updateOnlyChanged = true;
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+}
